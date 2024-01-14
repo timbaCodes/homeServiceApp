6edd425e494bf153bf3e4a7ed163b37cce049154
Firas Rebai <Rebaifiras864@gmail.com>
2024-01-14 01:44:35 +0100

    https://clerk.com/docs/quickstarts/expo---'scheme': 'myapp'

--- App/Screens/LoginScreen/Loginx.jsx
+++ App/Screens/LoginScreen/Loginx.jsx
@@ -1,79 +1,29 @@
-import { useSignUp, useSignIn } from "@clerk/clerk-expo";
+import React from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import Colors from "../../Utils/Colors";
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
+import { useWarmUpBrowser } from "../../hooks/warmUpBrowser";
 
-import * as AuthSession from "expo-auth-session";
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Loginx() {
-  const { isLoaded, signIn, setSession } = useSignIn();
-  const { signUp } = useSignUp();
-  if (!isLoaded) return null;
-
-  const handleSignInWithDiscordPress = async () => {
+  useWarmUpBrowser();
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+  const onPress = React.useCallback(async () => {
     try {
-      const redirectUrl = AuthSession.makeRedirectUri({
-        path: "/oauth-native-callback",
-      });
-
-      // Choose your OAuth provider, based upon your instance.
-      await signIn.create({
-        strategy: "oauth_google",
-        redirectUrl,
-      });
-
-      const {
-        firstFactorVerification: { externalVerificationRedirectURL },
-      } = signIn;
-
-      if (!externalVerificationRedirectURL)
-        throw "Something went wrong during the OAuth flow. Try again.";
-
-      const authResult = await AuthSession.startAsync({
-        authUrl: externalVerificationRedirectURL.toString(),
-        returnUrl: redirectUrl,
-      });
-
-      if (authResult.type !== "success") {
-        throw "Something went wrong during the OAuth flow. Try again.";
-      }
-
-      // Get the rotatingTokenNonce from the redirect URL parameters
-      const { rotating_token_nonce: rotatingTokenNonce } = authResult.params;
-
-      await signIn.reload({ rotatingTokenNonce });
-
-      const { createdSessionId } = signIn;
-
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+ 
       if (createdSessionId) {
-        // If we have a createdSessionId, then auth was successful
-        await setSession(createdSessionId);
+        setActive({ session: createdSessionId });
       } else {
-        // If we have no createdSessionId, then this is a first time sign-in, so
-        // we should process this as a signUp instead
-        // Throw if we're not in the right state for creating a new user
-        if (
-          !signUp ||
-          signIn.firstFactorVerification.status !== "transferable"
-        ) {
-          throw "Something went wrong during the Sign up OAuth flow. Please ensure that all sign up requirements are met.";
-        }
-
-        console.log(
-          "Didn't have an account transferring, following through with new account sign up"
-        );
-
-        // Create user
-        await signUp.create({ transfer: true });
-        await signUp.reload({
-          rotatingTokenNonce: authResult.params.rotating_token_nonce,
-        });
-        await setSession(signUp.createdSessionId);
+        // Use signIn or signUp for next steps such as MFA
       }
     } catch (err) {
-      console.log(JSON.stringify(err, null, 2));
-      console.log("error signing in", err);
+      console.error("OAuth error", err);
     }
-  };
+  }, []);
 
   return (
     <View
@@ -107,7 +57,7 @@ export default function Loginx() {
         </Text>
         <TouchableOpacity
           style={styles.button}
-          onPress={handleSignInWithDiscordPress}
+          onPress={onPress}
         >
           <Text
             style={{ textAlign: "center", fontSize: 17, color: Colors.PRIMARY }}
